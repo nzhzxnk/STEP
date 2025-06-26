231f3bc4d01e6074d091a5b53fb8d6ff28ec3a22
Hayashi Ayano <haayashi.ayano.s1@dc.tohoku.ac.jp>
2025-06-27 00:35:49 +0900

    update homework summary

--- week5_homework/output_generator.py
+++ week5_homework/output_generator.py
@@ -12,7 +12,7 @@ def generate_sample_output():
     for i in range(CHALLENGES):
         address_list = read_input(f'input_{i}.csv')
         tour = solver_taboo.solve(address_list) # 初期経路を取得
-        iteration_num , max_length_taboo = 50, 5 #  iteration_numは改善を行う回数、max_length_tabooはタブーリストの大きさ
+        iteration_num , max_length_taboo = 750, 20 #  iteration_numは改善を行う回数、max_length_tabooはタブーリストの大きさ
         best_tour = solver_taboo.taboo_search(tour, address_list, iteration_num, max_length_taboo)
         with open(f'output_{i}.csv', 'w') as f:
             f.write(format_tour(best_tour) + '\n')
